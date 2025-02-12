78944e4f3968edd2d383adb7f8fd4c576d8e8bbf
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-02-12 18:33:29 +0000

    Remove

--- conanfile.py
+++ conanfile.py
@@ -1,65 +1,16 @@
 import os
 
 from conan import ConanFile
-from conan.tools.files import copy
 from conan.tools.scons import SConsDeps
 from conan.tools.layout import basic_layout
 
 
 class PaginatorConan(ConanFile):
-    name = "paginator"
-    description = "Extensible paginator class."
-    version = "0.0.0"
-    license = "MIT"
-
-    url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/JoelLefkowitz/paginator"
-
-    topics = (
-        "cache",
-        "fifo",
-        "lifo",
-        "lru",
-        "header-only",
-    )
-
-    package_type = "header-library"
-    settings = (
-        "os",
-        "arch",
-        "compiler",
-        "build_type",
-    )
-
-    no_copy_source = True
-    exports = "src/*"
-
     def layout(self):
         basic_layout(self, src_folder="src")
 
-    def package_id(self):
-        self.info.clear()
+    def generate(self):
+        SConsDeps(self).generate()
 
     def build_requirements(self):
         self.test_requires("gtest/1.12.1")
-
-    def build(self):
-        pass
-
-    def package(self):
-        copy(
-            self,
-            "LICENSE.md",
-            self.recipe_folder,
-            os.path.join(self.package_folder, "licenses"),
-        )
-        copy(
-            self,
-            "*.[ht]pp",
-            os.path.join(self.recipe_folder, "src"),
-            os.path.join(self.package_folder, "include", self.name),
-        )
-
-    def package_info(self):
-        self.cpp_info.bindirs = []
-        self.cpp_info.libdirs = []
--- test_package/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <paginator/paginator.tpp>
-#include <vector>
-
-int main() {
-    paginator::Paginator<int> paginator({1, 2, 3, 4, 5}, 2);
-    return 0;
-}
