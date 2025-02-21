088074a30c678c5aa6374a9975e440c7b74a625d
Joel Lefkowitz <joel.lefkowitz@teya.com>
2025-02-21 11:18:14 +0000

    Add publishing conanfile source method

--- SConstruct.py
+++ SConstruct.py
@@ -10,15 +10,13 @@ from miniscons import (
 )
 from walkmate import tree
 
-conandeps = "build/conan/SConscript_conandeps"
-
-env = conan(source=conandeps)
+env = conan()
 
 tests = Build(
     "tests",
     tree("src", r"\.cpp$"),
     flags("c++11"),
-    packages(["gtest"], source=conandeps),
+    packages(["gtest"]),
 )
 
 test = Target(
